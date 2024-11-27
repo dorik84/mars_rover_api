05a643848aeb122064104d7519d0fa2c28744a05
dorik84 <dorya2005@mail.ru>
2024-11-27 19:42:08 -0400

    Initial commit

---
+++ src/main/java/ca/odoroshchuk/mars_rover_api/MarsRoverApiApplication.java
@@ -0,0 +1,13 @@
+package ca.odoroshchuk.mars_rover_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarsRoverApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarsRoverApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ca/odoroshchuk/mars_rover_api/MarsRoverApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ca.odoroshchuk.mars_rover_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarsRoverApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
